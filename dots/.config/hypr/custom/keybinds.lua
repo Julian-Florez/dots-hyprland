a099647ec19b52275f7bc3310c4291910f67ca7f
Julian-Florez <jflorez33453@universidadean.edu.co>
2026-08-10 22:33:31 -0500

    fix(hyprland): add locked = true option to touchpad keybind

--- dots/.config/hypr/custom/keybinds.lua
+++ dots/.config/hypr/custom/keybinds.lua
@@ -10,7 +10,7 @@ hl.bind("Num_Lock", hl.dsp.exec_cmd("/home/julian/.local/bin/numlock-toggle"), {
 hl.bind("Scroll_Lock", hl.dsp.exec_cmd("/home/julian/.local/bin/scrolllock-toggle"), { non_consuming = true, description = "Indicators: ScrollLock" })
 
 hl.bind("SUPER + F9", hl.dsp.exec_cmd("/home/julian/.local/bin/touchpad-toggle"), { description = "Indicators: Toggle Touchpad" })
-hl.bind("XF86TouchpadToggle", hl.dsp.exec_cmd("/home/julian/.local/bin/touchpad-toggle"), { description = "Indicators: Toggle Touchpad" })
+hl.bind("XF86TouchpadToggle", hl.dsp.exec_cmd("/home/julian/.local/bin/touchpad-toggle"), { locked = true, description = "Indicators: Toggle Touchpad" })
 
 hl.bind("SUPER + F4", hl.dsp.exec_cmd("/home/julian/.local/bin/micmute-toggle"), { description = "Indicators: Toggle Mic Mute" })
 hl.bind("XF86AudioMicMute", hl.dsp.exec_cmd("/home/julian/.local/bin/micmute-toggle"), { locked = true, description = "Indicators: Toggle Mic Mute" })
